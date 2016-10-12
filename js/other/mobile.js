d8a17512f444dc72b558562e5228ad54018cfe40
nununoisy <nununoisy@gmail.com>
2016-10-12 12:45:44 -0700

    Update mobile.js

--- js/other/mobile.js
+++ js/other/mobile.js
@@ -42,7 +42,7 @@ function loadViaMozActivity () {
 
 function loadViaXHR () {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "../pokeblue.gb");
+  xhr.open("GET", "pokeblue.gb");
   xhr.responseType = "blob";
   xhr.onload = function () {
     startGame(new Blob([this.response], { type: "text/plain" }));
