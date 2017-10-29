cc001746b64472f56672c9a8823d04217fc33711
Noah <9949555+nununoisy@users.noreply.github.com>
2017-10-29 15:28:37 -0700

    Remove debug menu, fix saving

--- js/other/mobile.js
+++ js/other/mobile.js
@@ -59,6 +59,7 @@ function loadViaFileInput() {
 	input.style.display = "none";
 	if (input.files.length === 0) return;
 	var rom = input.files[0];
+    setInterval(saveSRAM(), 5000);
 	startGame(rom);
 }
 
